e2dccfc76fce763fa0306e91c221b43da9a96f41
Atanas Atanasov <scriptex.bg@gmail.com>
2018-08-18 14:06:14 +0300

    Update build files

--- dist/index.js
+++ dist/index.js
@@ -1,214 +1,123 @@
-(function webpackUniversalModuleDefinition(root, factory) {
-	if(typeof exports === 'object' && typeof module === 'object')
-		module.exports = factory();
-	else if(typeof define === 'function' && define.amd)
-		define([], factory);
-	else if(typeof exports === 'object')
-		exports["html5formValidation"] = factory();
-	else
-		root["html5formValidation"] = factory();
-})(window, function() {
-return /******/ (function(modules) { // webpackBootstrap
-/******/ 	// The module cache
-/******/ 	var installedModules = {};
-/******/
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId]) {
-/******/ 			return installedModules[moduleId].exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			i: moduleId,
-/******/ 			l: false,
-/******/ 			exports: {}
-/******/ 		};
-/******/
-/******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
-/******/ 		// Flag the module as loaded
-/******/ 		module.l = true;
-/******/
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/
-/******/
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = modules;
-/******/
-/******/ 	// expose the module cache
-/******/ 	__webpack_require__.c = installedModules;
-/******/
-/******/ 	// define getter function for harmony exports
-/******/ 	__webpack_require__.d = function(exports, name, getter) {
-/******/ 		if(!__webpack_require__.o(exports, name)) {
-/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
-/******/ 		}
-/******/ 	};
-/******/
-/******/ 	// define __esModule on exports
-/******/ 	__webpack_require__.r = function(exports) {
-/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 		}
-/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 	};
-/******/
-/******/ 	// create a fake namespace object
-/******/ 	// mode & 1: value is a module id, require it
-/******/ 	// mode & 2: merge all properties of value into the ns
-/******/ 	// mode & 4: return value when already ns object
-/******/ 	// mode & 8|1: behave like require
-/******/ 	__webpack_require__.t = function(value, mode) {
-/******/ 		if(mode & 1) value = __webpack_require__(value);
-/******/ 		if(mode & 8) return value;
-/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
-/******/ 		var ns = Object.create(null);
-/******/ 		__webpack_require__.r(ns);
-/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
-/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
-/******/ 		return ns;
-/******/ 	};
-/******/
-/******/ 	// getDefaultExport function for compatibility with non-harmony modules
-/******/ 	__webpack_require__.n = function(module) {
-/******/ 		var getter = module && module.__esModule ?
-/******/ 			function getDefault() { return module['default']; } :
-/******/ 			function getModuleExports() { return module; };
-/******/ 		__webpack_require__.d(getter, 'a', getter);
-/******/ 		return getter;
-/******/ 	};
-/******/
-/******/ 	// Object.prototype.hasOwnProperty.call
-/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
-/******/
-/******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
-/******/
-/******/
-/******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
-/******/ })
-/************************************************************************/
-/******/ ([
-/* 0 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = void 0;
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-var html5formValidation =
-/*#__PURE__*/
-function () {
-  function html5formValidation() {
-    var element = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : document.querySelector('form');
-    var settings = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
-
-    _classCallCheck(this, html5formValidation);
-
-    this.settings = Object.assign({}, {
-      errorElement: 'error',
-      invalidClass: 'invalid',
-      submitHandler: null,
-      validateOnInput: true
-    }, settings);
-    this.form = element;
-    this.errorDiv = "<div class=\"".concat(this.settings.errorElement, "\"></div>");
-    this.init();
+(function (global, factory) {
+  if (typeof define === "function" && define.amd) {
+    define(["exports"], factory);
+  } else if (typeof exports !== "undefined") {
+    factory(exports);
+  } else {
+    var mod = {
+      exports: {}
+    };
+    factory(mod.exports);
+    global.index = mod.exports;
   }
+})(this, function (_exports) {
+  "use strict";
 
-  _createClass(html5formValidation, [{
-    key: "init",
-    value: function init() {
-      var _this = this;
+  Object.defineProperty(_exports, "__esModule", {
+    value: true
+  });
+  _exports.default = void 0;
 
-      this.form.noValidate = true;
-      this.settings.validateOnInput && this.validateAll(this.form);
+  function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-      this.form.onsubmit = function (event) {
-        _this.validateAll(_this.form);
+  function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
-        if (!_this.form.checkValidity()) {
-          event.preventDefault();
-        } else {
-          if (typeof _this.settings.submitHandler === 'function') {
+  function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+  var html5formValidation =
+  /*#__PURE__*/
+  function () {
+    function html5formValidation() {
+      var element = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : document.querySelector('form');
+      var settings = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+
+      _classCallCheck(this, html5formValidation);
+
+      this.settings = Object.assign({}, {
+        errorElement: 'error',
+        invalidClass: 'invalid',
+        submitHandler: null,
+        validateOnInput: true
+      }, settings);
+      this.form = element;
+      this.errorDiv = "<div class=\"".concat(this.settings.errorElement, "\"></div>");
+      this.init();
+    }
+
+    _createClass(html5formValidation, [{
+      key: "init",
+      value: function init() {
+        var _this = this;
+
+        this.form.noValidate = true;
+        this.settings.validateOnInput && this.validateAll(this.form);
+
+        this.form.onsubmit = function (event) {
+          _this.validateAll(_this.form);
+
+          if (!_this.form.checkValidity()) {
             event.preventDefault();
+          } else {
+            if (typeof _this.settings.submitHandler === 'function') {
+              event.preventDefault();
 
-            _this.settings.submitHandler(_this);
+              _this.settings.submitHandler(_this);
+            }
           }
-        }
-      };
-    }
-  }, {
-    key: "validateAll",
-    value: function validateAll(form) {
-      var requiredFields = form.querySelectorAll('[required]');
-      var _iteratorNormalCompletion = true;
-      var _didIteratorError = false;
-      var _iteratorError = undefined;
+        };
+      }
+    }, {
+      key: "validateAll",
+      value: function validateAll(form) {
+        var requiredFields = form.querySelectorAll('[required]');
+        var _iteratorNormalCompletion = true;
+        var _didIteratorError = false;
+        var _iteratorError = undefined;
 
-      try {
-        for (var _iterator = requiredFields[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-          var field = _step.value;
-          this.validateField(field);
-        }
-      } catch (err) {
-        _didIteratorError = true;
-        _iteratorError = err;
-      } finally {
         try {
-          if (!_iteratorNormalCompletion && _iterator.return != null) {
-            _iterator.return();
+          for (var _iterator = requiredFields[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+            var field = _step.value;
+            this.validateField(field);
           }
+        } catch (err) {
+          _didIteratorError = true;
+          _iteratorError = err;
         } finally {
-          if (_didIteratorError) {
-            throw _iteratorError;
+          try {
+            if (!_iteratorNormalCompletion && _iterator.return != null) {
+              _iterator.return();
+            }
+          } finally {
+            if (_didIteratorError) {
+              throw _iteratorError;
+            }
           }
         }
       }
-    }
-  }, {
-    key: "validateField",
-    value: function validateField(field) {
-      var _this2 = this;
+    }, {
+      key: "validateField",
+      value: function validateField(field) {
+        var _this2 = this;
 
-      if (!(field.nextSibling.classList && field.nextSibling.classList.contains(this.settings.errorElement))) {
-        field.insertAdjacentHTML('afterend', this.errorDiv);
+        if (!(field.nextSibling.classList && field.nextSibling.classList.contains(this.settings.errorElement))) {
+          field.insertAdjacentHTML('afterend', this.errorDiv);
+        }
+
+        field.oninvalid = function () {
+          field.classList.add(_this2.settings.invalidClass);
+          field.nextSibling.textContent = field.validationMessage;
+        };
+
+        field.oninput = function () {
+          field.nextSibling.textContent = '';
+          field.classList.remove(_this2.settings.invalidClass);
+          field.checkValidity();
+        };
       }
+    }]);
 
-      field.oninvalid = function () {
-        field.classList.add(_this2.settings.invalidClass);
-        field.nextSibling.textContent = field.validationMessage;
-      };
+    return html5formValidation;
+  }();
 
-      field.oninput = function () {
-        field.nextSibling.textContent = '';
-        field.classList.remove(_this2.settings.invalidClass);
-        field.checkValidity();
-      };
-    }
-  }]);
-
-  return html5formValidation;
-}();
-
-exports.default = html5formValidation;
-
-/***/ })
-/******/ ])["default"];
+  _exports.default = html5formValidation;
 });
